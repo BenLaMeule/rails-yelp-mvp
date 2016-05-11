5ac48ece2705a9232890959d4a845156eb5e9b19
Benjamin Meuleman <benjamin.meuleman@gmail.com>
2016-05-11 12:15:48 +0200

    add model validation

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,3 +1,7 @@
 class Restaurant < ActiveRecord::Base
-  has_many :reviews
+  CATEGORIES = ["chinese", "italian", "japanese", "french", "belgian"]
+  # When a restaurant is destroyed, all reviews should be destroyed as well.
+  has_many :reviews, dependent: :destroy
+  validates :name, :address, presence: true
+  validates :category, inclusion: { in: CATEGORIES, allow_nil: false }
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,8 @@
 class Review < ActiveRecord::Base
   belongs_to :restaurant
+  # A review must have a content and a rating. The rating should be a number between 0 and 5.
+  validates :rating, presence: true, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 5 }
+  validates :content, presence: true
+  # A review must have a parent restaurant.
+  validates_presence_of :restaurant
 end
